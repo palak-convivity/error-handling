4b1d3154dafabbaa0f84c254ea754ae68b188f2e
palak-convivity <palak@convivity.com>
2024-08-13 13:32:44 +0530

    my changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
-export default async function Home() {
-  const response = await fetch("http://localhost:3002/api/home", { cache: "no-cache" })
-  let data = {}
-  if (response.status !== 200) { console.log("res>>>>>>>>>>", response) } else { data = await response.json() }
+'use client';
+import { toast } from 'sonner';
+import useSWR from 'swr';
+
+export default function Page() {
+  const { data, error, isLoading } = useSWR('/api/home', fetcher, {
+    onError: () => {
+      //before error
+      alert('something went wrong');
+      // toast('something went wrong');
+    },
+  });
+  console.log(data, error);
+
+  if (!isLoading && error) {
+    //after error
+    //alert(error.message);
+    // toast(error?.message);
+    return (
+      <main className='flex min-h-screen flex-col items-center justify-between p-24 '>
+        {error?.message}
+      </main>
+    );
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {JSON.stringify(data)}
+    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-white text-black'>
+      {isLoading ? <>loading...</> : JSON.stringify(data)}
     </main>
   );
 }
+
+const fetcher = async (url: string) => {
+  const response = await fetch(`http://localhost:3002${url}`, {
+    cache: 'no-cache',
+  });
+  let data = {};
+  if (response.status !== 200) {
+    throw new Error(response.statusText);
+  } else {
+    data = await response.json();
+  }
+  return data;
+};
